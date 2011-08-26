f261e2e2c8124353350163206adbc7cc2a801e33
Pacha Camac <pachacamac@lavabit.com>
2011-08-26 15:16:06 +0200

    this is where it begins

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1>))) COherent_LIabilities (((</h1>
+Displays your coherent liabilities .. everybody has them .. everybody gets fucked by them once in a while .. stay on top!
+
+<h3>Problem:</h3>
+If you are .. well .. a person .. then you have things like an addresses, a telephone number or insurances.
+
+Every once in a while you change some of these things and you almost always forget some of the things that depend on the thing you just changed.
+
+For example: Maybe your cellphone provider needs to know your email address and your bank account. Now you change your bank and there will be a very good chance you forgot to change the account information at your cellphone provider. The result will be a dunning letter. That was the easy case. If you ever moved to a new city you know what kind of trouble this can be.
+
+<h3>Solution:</h3>
+
+Use this little script to keep track of the coherences between your liabilities and see at a glance what you need to do in such a scenario!
+
+<h2>Requirements</h2>
+* package <b>graphviz</b> (e.g. apt-get install graphviz)
+
+<h2>Usage</h2>
+`ruby coli.rb`
+
---
+++ coli.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+liabilities = [
+  'address', 'bank account', 'employer', 'landlord',
+  'health insurance', 'liability insurance',
+  'internet provider', 'telephone number', 'cellphone number',
+  'email address', 'web domain',
+  'twitter account', 'facebook account', 'reddit account'
+]
+graph = {}
+
+def yes?; gets.chars.first.downcase == 'y'; end
+
+liabilities.each_with_index do |liability,idx|
+  print "#{idx+1}/#{liabilities.size} Do you have a #{liability}? (y/n) "
+  graph[liability] = [] if yes?
+end
+
+loop do
+  print "Do you want to add another liability? (y/n) "
+  break unless yes?
+  print "What liability do you want to add? "
+  graph[gets.strip] = []
+end
+
+graph.keys.each do |liability1|
+  graph.keys.each do |liability2|
+    next if liability1 == liability2
+    print "Does your #{liability1} need your #{liability2}? (y/n) "
+    graph[liability1] << liability2 if yes?
+  end
+end
+
+File.open('coli.dot','w') do |f|
+  f.puts 'digraph coli {'
+  graph.each_pair do |liability1, liabilities|
+    liabilities.each do |liability2|
+      f.puts "\"#{liability1}\" -> \"#{liability2}\";"
+    end
+  end
+  f.puts '}'
+end
+
+puts 'All done! Generating output ... coli.dot, coli_dot.png, coli_circo.png'
+`dot -Tpng coli.dot -ocoli_dot.png && circo -Tpng coli.dot -ocoli_circo.png`
+
